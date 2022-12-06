3dbd3fe00f87852a4582de5b8984da9220268e51
Your Name <jaiprakashpatel18@navgurukul.org>
2022-12-06 12:03:13 +0530

    first commit

--- src/componets/Portfolio/Portfolio.js
+++ src/componets/Portfolio/Portfolio.js
@@ -15,7 +15,7 @@ var data = [
     title:
       "Everhour is a time tracker website that helps small and mid-size companies  to  organize their working hours.",
     github: "https://github.com/jaypatel149",
-    demo: "https://ever-lmnstkh00-paruljamwal.vercel.app",
+    demo: "https://jaypatel149.github.io/shopping_mall/",
   },
 
   {
@@ -24,21 +24,21 @@ var data = [
     title:
       "OverStock is a American product based organisation where you can buy home decor, furniture etc.",
     github: "https://github.com/jaypatel149",
-    demo: "https://over-bz1zdp857-paruljamwal.vercel.app",
+    demo: "https://jaypatel149.github.io/shopping_mall/",
   },
   {
     id: 3,
     image: IMG4,
     title: "LifeStyleStores is a Product based website",
     github: "https://github.com/jaypatel149",
-    demo: "https://over-bz1zdp857-paruljamwal.vercel.app/",
+    demo: "https://jaypatel149.github.io/Myntra_website/",
   },
   {
     id: 4,
     image: IMG1,
     title: "BEST BUY is a Electronic products based web.",
     github: "https://github.com/jaypatel149",
-    demo: "https://over-bz1zdp857-paruljamwal.vercel.app/",
+    demo: "https://jaypatel149.github.io/Electronic_web/",
   },
 
   {
@@ -46,14 +46,14 @@ var data = [
     image: IMG2,
     title: "FRESHLY is an organization who delivers food in different areas.",
     github: "https://github.com/jaypatel149",
-    demo: "https://over-bz1zdp857-paruljamwal.vercel.app/",
+    demo: "https://jaypatel149.github.io/Vaid_G_UI/",
   },
   {
     id: 6,
     image: IMG3,
     title: "Ecommerce web. provides clothes and Major know for Footwear.",
     github: "https://github.com/jaypatel149",
-    demo: "https://over-bz1zdp857-paruljamwal.vercel.app/",
+    demo: "https://jaypatel149.github.io/Vaid_G_UI/",
   },
 ];
 
