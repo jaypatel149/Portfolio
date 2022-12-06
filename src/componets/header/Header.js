3dca07a189d07f7de47d9370c232a8441a3029eb
Your Name <jaiprakashpatel18@navgurukul.org>
2022-12-06 13:18:10 +0530

    first commit

--- src/componets/header/Header.js
+++ src/componets/header/Header.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { CTA } from "./CTA";
-import { HeaderSocial } from "./HeaderSocial";
 import ME from "../../assets/jay1.jpg";
-import './Header.css';
-import { Box, Link, Typography, Container } from "@mui/material";
-
-
+import "./Header.css";
+import { Box, Typography, Container, Grid } from "@mui/material";
 
 function Header() {
   return (
-      <Container sx={{ height: '150vh', paddingTop:'4rem',overflow:'hidden'}}>
-        <Box sx={{textAlign:'center',height:'100%',position:'relative'}}>
-          <Typography variant="h6" mb={1} >Hello 👋🏻 I'm</Typography>
-          <Typography variant="h4" my={1}>Jay Patel</Typography>
-          <Typography variant="h5" mb={1}>Frontend Developer &nbsp;(React Js)</Typography>
-          <CTA />
-          <HeaderSocial />
-          <Box className="me" >
+    <Container>
+      <Grid container spacing={3} py={5}>
+        <Grid item xs={12} lg={6}>
+          <Box sx={{ textAlign: "center"}}>
+            <Typography variant="h6" mb={1}>
+              Hello 👋🏻 I'm
+            </Typography>
+            <Typography variant="h4" my={1}>
+              Jay Patel
+            </Typography>
+            <Typography variant="h5" mb={1}>
+              Frontend Developer &nbsp;(React Js)
+            </Typography>
+            <CTA />
+          </Box>
+        </Grid>
+        <Grid item xs={12} lg={6}>
+          <Box className="me">
             <img src={ME} alt="mine" />
           </Box>
-          <Link href="#contact" underline="none" color='white' className="scroll__down">Scroll down</Link>
-        </Box>
-      </Container>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
